b612cf85c5106af416929570aab9ee2d7dd02074
Jamund Ferguson <jamund@gmail.com>
2020-12-25 15:38:05 -0800

    a bit cleaner

--- src/components/AmountField.js
+++ src/components/AmountField.js
@@ -1,8 +1,8 @@
 import { useSelector, useDispatch } from "react-redux";
 
 export function AmountField() {
-  const amount = useSelector((state) => state.amount);
   const dispatch = useDispatch();
+  const amount = useSelector((state) => state.amount);
   function onChange(e) {
     let newAmount = e.target.value;
     dispatch({ type: "amountChanged", payload: newAmount });
--- src/components/RateTable.js
+++ src/components/RateTable.js
@@ -1,13 +1,24 @@
-import { RateRow } from "./RateRow";
+import { useSelector } from "react-redux";
 
-export function RateTable({ currencyData, amount }) {
+export function RateTable({ currencyData }) {
+  const amount = useSelector((state) => state.amount);
   return (
     <table className="ExchangeRate-table">
       <tbody>
         {Object.entries(currencyData).map(([code, rate]) => {
           // NOTE: normally avoid floating point math in JS
           const exchangeAmount = amount * rate || 0.0;
-          return <RateRow amount={exchangeAmount} code={code} />;
+          return (
+            <tr>
+              <td>{code}</td>
+              <td>
+                {exchangeAmount.toLocaleString("en", {
+                  style: "currency",
+                  currency: code,
+                })}
+              </td>
+            </tr>
+          );
         })}
       </tbody>
     </table>
