1485f531633456c2a7b1c23b82963121e78b1b7a
Jamund Ferguson <jamund@gmail.com>
2021-05-15 10:41:14 -0700

    added error handling and a mock API because the one we used stopped being free

--- src/api.js
+++ src/api.js
@@ -1,3 +1,8 @@
+// because the API we were using stopped being free
+// we've added this mock fetch method, which supplies
+// its own exchange rate data
+import { fetch } from "./lib/fetch";
+
 // helper to make our exchange rate API call
 export function getExchangeRates(base, currencies) {
   const codes = currencies
@@ -6,5 +11,20 @@ export function getExchangeRates(base, currencies) {
   const url = `https://api.exchangeratesapi.io/latest?base=${base}&symbols=${codes}`;
   return fetch(url)
     .then((res) => res.json())
+    .then(handleAPIErrors)
     .then((res) => res.rates);
+}
+
+// if there was an error log it and return super basic mock data
+// errors for this API came back in the form of { success: false, error: {...}}
+// so we can check for that and log any errors and then respond with a basic
+// valid response so the app doesn't crash
+function handleAPIErrors(res) {
+  if (res.success) return res;
+  console.error(`Server Error: ${res.error.info}`);
+  return {
+    rates: {
+      USD: 1.0,
+    },
+  };
 }
