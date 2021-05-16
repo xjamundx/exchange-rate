53d0d01fedc1c050ad37573b09c1e35687e882ff
Jamund Ferguson <jamund@gmail.com>
2021-05-15 18:40:19 -0700

    fixed a bug with a missing useDispatch hook

--- src/components/ExchangeRate.js
+++ src/components/ExchangeRate.js
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
-import { dispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { updateCurrencyCode } from "../reducers/RateReducer";
 import { RateTable } from "./RateTable";
 import { CurrencyCodePicker } from "./CurrencyCodePicker";
 import { AmountField } from "./AmountField";
 
 export function ExchangeRate() {
+  const dispatch = useDispatch();
   useEffect(() => {
     dispatch(updateCurrencyCode());
   }, []);
